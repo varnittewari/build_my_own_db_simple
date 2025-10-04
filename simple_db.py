9bc0e033305c2350987e39e67c8798de36efc384
Varnit Tewari <varnittewari@Varnits-MacBook-Pro.local>
2025-10-04 17:48:17 -0400

    scaffolding

---
+++ simple_db.py
@@ -0,0 +1,62 @@
+from gevent import socket
+from gevent.pool import Pool
+from gevent.server import StreamServer
+
+from collections import namedtuple
+from io import BytesIO
+from socket import error as socket_error
+
+
+# We'll use exceptions to notify the connection-handling loop of problems.
+class CommandError(Exception): pass
+class Disconnect(Exception): pass
+
+Error = namedtuple('Error', ('message',))
+
+
+class ProtocolHandler(object):
+    def handle_request(self, socket_file):
+        # Parse a request from the client into it's component parts.
+        pass
+
+    def write_response(self, socket_file, data):
+        # Serialize the response data and send it to the client.
+        pass
+
+
+class Server(object):
+    def __init__(self, host='127.0.0.1', port=31337, max_clients=64):
+        self._pool = Pool(max_clients)
+        self._server = StreamServer(
+            (host, port),
+            self.connection_handler,
+            spawn=self._pool)
+
+        self._protocol = ProtocolHandler()
+        self._kv = {}
+
+    def connection_handler(self, conn, address):
+        # Convert "conn" (a socket object) into a file-like object.
+        socket_file = conn.makefile('rwb')
+
+        # Process client requests until client disconnects.
+        while True:
+            try:
+                data = self._protocol.handle_request(socket_file)
+            except Disconnect:
+                break
+
+            try:
+                resp = self.get_response(data)
+            except CommandError as exc:
+                resp = Error(exc.args[0])
+
+            self._protocol.write_response(socket_file, resp)
+
+    def get_response(self, data):
+        # Here we'll actually unpack the data sent by the client, execute the
+        # command they specified, and pass back the return value.
+        pass
+
+    def run(self):
+        self._server.serve_forever()
